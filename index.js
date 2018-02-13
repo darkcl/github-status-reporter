a91d5f851e8d76f097c7db3481b9271dd006c634
Yeung Yiu Hung <hkclex@gmail.com>
2018-02-13 17:38:19 +0800

    Add host support

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ argv.repo = argv.repo || process.env.GITHUB_REPO;
 argv.state = argv.state || process.env.GITHUB_STATE;
 argv['target-url'] = argv['target-url'] || process.env.GITHUB_TARGET_URL;
 argv.description = argv.description || process.env.GITHUB_DESCRIPTION;
+argv['api-endpoint'] = argv['api-endpoint'] || process.env.GITHUB_API_ENDPOINT;
 argv.context = argv.context || process.env.GITHUB_CONTEXT;
 
 var query;
 var StatusReporter = require('./lib');
-var statusReporter = new StatusReporter(argv.token);
+var statusReporter = new StatusReporter(argv.token, argv['api-endpoint']);
 statusReporter.update(argv, function (err, res) {
   if (err) {
     console.error(err);
